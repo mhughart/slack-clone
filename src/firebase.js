64626aeceafe6c9e9b505c1f955e1527ba1c49e9
Michael Hughart <michael.hughart@rabisudesign.com>
2020-09-13 11:14:03 -0400

    Add Firebase

---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBS3SrK8bf_oeDs71MhhsNAkxhGweUT1pw",
+  authDomain: "slack-clone-323c3.firebaseapp.com",
+  databaseURL: "https://slack-clone-323c3.firebaseio.com",
+  projectId: "slack-clone-323c3",
+  storageBucket: "slack-clone-323c3.appspot.com",
+  messagingSenderId: "881246900235",
+  appId: "1:881246900235:web:6eb7acd188f0dfe195b70e"
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+
+export { auth, provider };
+export default db;
