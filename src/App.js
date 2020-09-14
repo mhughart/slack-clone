066dc2c5dd34bcd57eb2d31ca0caef70dc89f129
mhughart <56726808+mhughart@users.noreply.github.com>
2020-09-13 20:48:57 -0400

    Updates

--- src/App.js
+++ src/App.js
@@ -2,19 +2,27 @@ import React from "react";
 import "./App.css";
 import Header from "./Header";
 import Sidebar from "./Sidebar";
+import Chat from "./Chat";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
   return (
     // BEM naming convention
-    <div className='App'>
-      <h1>Slack-Clone Build</h1>
-
-      <Header />
-      <div className='app__body'>
-        <Sidebar />
-      </div>
-
-      {/* React-Router -> Chat Screem */}
+    <div className="App">
+      <Router>
+        <Header />
+        <div className="app__body">
+          <Sidebar />
+          <Switch>
+            <Route path="/room/:roomId">
+              <Chat />
+            </Route>
+            <Route path="/">
+              <h1>Welcome</h1>
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 }
