b66b04c0e67f05b595c6bfd5c690ae8d0c8352d4
Michael Hughart <michael.hughart@rabisudesign.com>
2020-09-15 12:47:13 -0400

    google auth install

--- src/Login.js
+++ src/Login.js
@@ -1,8 +1,27 @@
 import React from 'react';
 import './Login.css';
 import { Button } from '@material-ui/core';
+import { auth, provider } from './firebase';
+import { useStateValue } from './StateProvider';
+import { actionTypes } from './reducer';
 
 function Login() {
+	const [state, dispatch] = useStateValue();
+
+	const signIn = () => {
+		auth.signInWithPopup(provider)
+			.then((result) => {
+				console.log(result);
+				dispatch({
+					type: actionTypes.SET_USER,
+					user: result.user,
+				});
+			})
+			.catch((error) => {
+				alert(error.message);
+			});
+	};
+
 	return (
 		<div className='login'>
 			<div className='login__container'>
@@ -10,9 +29,9 @@ function Login() {
 					src='https://www.logo.wine/a/logo/Slack_Technologies/Slack_Technologies-Mark-Black-Logo.wine.svg'
 					alt=''
 				/>
-				<h1>Sign into Rabisu Design's Slack Clone</h1>
+				<h1>Rabisu Design's Slack Clone</h1>
 				<p>rabisudesign.slack.com</p>
-				<Button>Sign In with Google</Button>
+				<Button onClick={signIn}>Sign In with Google</Button>
 			</div>
 		</div>
 	);
