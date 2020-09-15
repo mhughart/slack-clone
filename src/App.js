5e387ca130cc7179bd52316eb07489f91f969d87
mhughart <56726808+mhughart@users.noreply.github.com>
2020-09-14 21:22:57 -0400

    Updates

--- src/App.js
+++ src/App.js
@@ -1,31 +1,31 @@
-import React from 'react';
-import './App.css';
-import Header from './Header';
-import Sidebar from './Sidebar';
-import Chat from './Chat';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import React from "react";
+import "./App.css";
+import Header from "./Header";
+import Sidebar from "./Sidebar";
+import Chat from "./Chat";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
-	return (
-		// BEM naming convention
-		<div className='App'>
-			<Router>
-				<Header />
-				<div className='app__body'>
-					<Sidebar />
+  return (
+    // BEM naming convention
+    <div className="App">
+      <Router>
+        <Header />
+        <div className="app__body">
+          <Sidebar />
 
-					<Switch>
-						<Route path='/room/:roomId'>
-							<Chat />
-						</Route>
-						<Route path='/'>
-							<h1>Welcome</h1>
-						</Route>
-					</Switch>
-				</div>
-			</Router>
-		</div>
-	);
+          <Switch>
+            <Route path="/room/:roomId">
+              <Chat />
+            </Route>
+            <Route path="/">
+              <h1>Welcome</h1>
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
--- src/Chat.js
+++ src/Chat.js
@@ -1,40 +1,39 @@
-import React, { useState, useEffect } from 'react';
-import './Chat.css';
-import { useParams } from 'react-router-dom';
-import StarBorderOutlinedIcon from '@material-ui/icons/StarBorderOutlined';
-import InfoOutlinedIcon from '@material-ui/icons/InfoOutlined';
-import db from './firebase';
+import React, { useState, useEffect } from "react";
+import "./Chat.css";
+import { useParams } from "react-router-dom";
+import StarBorderOutlinedIcon from "@material-ui/icons/StarBorderOutlined";
+import InfoOutlinedIcon from "@material-ui/icons/InfoOutlined";
+import db from "./firebase";
 
 function Chat() {
-	const { roomId } = useParams();
-	const [roomDetails, setRoomDetails] = useState(null);
+  const { roomId } = useParams();
+  const [roomDetails, setRoomDetails] = useState(null);
 
-	useEffect(() => {
-		if (roomId) {
-			db.collection('rooms')
-				.doc(roomId)
-				.onSnapshot((snapshot) => setRoomDetails(snapshot.data()));
-		}
-	}, [roomId]);
+  useEffect(() => {
+    if (roomId) {
+      db.collection("rooms")
+        .doc(roomId)
+        .onSnapshot((snapshot) => setRoomDetails(snapshot.data()));
+    }
+  }, [roomId]);
 
-	return (
-		<div className='chat'>
-			<h2>You are in the {roomId} room</h2>
-			<div className='chat__header'>
-				<div className='chat__headerLeft'>
-					<h4 className='chat__channelName'>
-						<strong>#{roomDetails?.name}</strong>
-						<StarBorderOutlinedIcon />
-					</h4>
-				</div>
-				<div className='chat__headerRight'>
-					<p>
-						<InfoOutlinedIcon /> Details
-					</p>
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div className="chat">
+      <div className="chat__header">
+        <div className="chat__headerLeft">
+          <h4 className="chat__channelName">
+            <strong>#{roomDetails?.name}</strong>
+            <StarBorderOutlinedIcon />
+          </h4>
+        </div>
+        <div className="chat__headerRight">
+          <p>
+            <InfoOutlinedIcon /> Details
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Chat;
