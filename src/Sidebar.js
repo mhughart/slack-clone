020b056253dc2af3fffa4122026dfdd586110c26
Michael Hughart <michael.hughart@rabisudesign.com>
2020-09-22 07:13:07 -0400

    finalizations

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, Component } from 'react';
 import './Sidebar.css';
 import FiberManualRecordIcon from '@material-ui/icons/FiberManualRecord';
-import CreateIcon from '@material-ui/icons/Create';
+// import CreateIcon from '@material-ui/icons/Create';
 import InsertCommentIcon from '@material-ui/icons/InsertComment';
 import SidebarOption from './SidebarOption';
 import InboxIcon from '@material-ui/icons/Inbox';
@@ -15,7 +15,6 @@ import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import AddIcon from '@material-ui/icons/Add';
 import ExitToAppIcon from '@material-ui/icons/ExitToApp';
 import db from './firebase';
-import { auth, provider } from './firebase';
 import { useStateValue } from './StateProvider';
 import { Tooltip, IconButton } from '@material-ui/core';
 
@@ -57,7 +56,6 @@ function Sidebar() {
 			<SidebarOption Icon={PeopleAltIcon} title='People & User Groups' />
 			<SidebarOption Icon={AppsIcon} title='Apps' />
 			<SidebarOption Icon={FileCopyIcon} title='File Browser' />
-			<SidebarOption Icon={ExpandLessIcon} title='Show less' />
 
 			<hr />
 			<SidebarOption
